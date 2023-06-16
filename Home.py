841030d7399964f0edc92104230a94d4b6114d29
first.yuta <634230025@webmail.npru.ac.th>
2023-06-16 15:14:10 +0700

    test

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header('Pacharapon Yuwansiri and Samart Srichueasakunchai')
-st.image(".pic/me.jpg")
+#st.image(".pic/me.jpg")
 #st.image(".pic/samart.jpg")
 col1, col2, col3 = st.columns(3)
 
